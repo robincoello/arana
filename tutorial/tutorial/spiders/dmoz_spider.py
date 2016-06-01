072b33f67b48539538bc42c381929efcd8c5a6f1
robincoello <robincoello@debian>
2016-06-01 21:45:26 +0200

    add var in xml format

--- tutorial/tutorial/spiders/dmoz_spider.py
+++ tutorial/tutorial/spiders/dmoz_spider.py
@@ -1,10 +1,11 @@
 import scrapy
 from yattag import Doc, indent
-
+import csv
 
 class DmozSpider(scrapy.Spider):
     name = "dmoz"
     allowed_domains = []
+
 
     f = open("urls.txt")
     start_urls = [url.strip() for url in f.readlines()]
@@ -17,20 +18,32 @@ class DmozSpider(scrapy.Spider):
         print '---------------------------------------'
         print '---------------------------------------'
 # add a xml format
-        doc, tag, text = Doc().tagtext()
+# read the info
+        with open('subject_website.csv', 'rb') as f:
+            reader = csv.reader(f)
+            for row in reader:
+                w_id = row[0]
+                w_name = row[1]
+                w_info1 = row[2]
+                w_info2 = row[3]
+                w_url = row[4]
+                w_url_rss = 'http://www.myrss.com'
 
+        doc, tag, text = Doc().tagtext()
         with tag('root'):
             with tag('doc'):
                 with tag('id', name='web'):
-                    text('web')
+                    text(w_id)
                 with tag('name', name='rss'):
-                    text('qqq')
+                    text(w_name)
                 with tag('info1', name='rss'):
-                    text('qqq')
+                    text(w_info1)
                 with tag('info2', name='rss'):
-                    text('qqq')
+                    text(w_info2)
+                with tag('url', name='web_site'):
+                    text(w_url)
                 with tag('url', name='rss'):
-                    text('qqq')
+                    text(w_url_rss)
         result = indent(
             doc.getvalue(),
             indentation = ' '*4,
