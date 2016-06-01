72218c55b521caa908c1a5e4df9cd810680cdff3
robincoello <robincoello@debian>
2016-06-01 19:58:44 +0200

    cleaning

--- tutorial/tutorial/spiders/dmoz_spider.py
+++ tutorial/tutorial/spiders/dmoz_spider.py
@@ -1,5 +1,4 @@
 import scrapy
-from yattag import Doc, indent
 
 class DmozSpider(scrapy.Spider):
     name = "dmoz"
@@ -9,37 +8,17 @@ class DmozSpider(scrapy.Spider):
     start_urls = [url.strip() for url in f.readlines()]
     f.close()
  
-
     def parse(self, response):
 
 	#file to save
         filename = 'respuestas.html'
 	qqq = response.xpath("/html/head/link[@type='application/rss+xml']/@href").extract()        
         print '---------------------------------------'
-        doc, tag, text = Doc(
-            defaults = {
-            'webrss': qqq,
-            'url': 'You just won the lottery!'
-        },
-        ).tagtext()
-        with tag('root'):
-            with tag('doc'):
-                with tag('url', name='web'):
-                    text('webrss')
-                with tag('url', name='rss'):
-                    text('qqq')
-        result = indent(
-            doc.getvalue(),
-            indentation = ' '*4,
-            newline = '\r\n'
-        )
-
         print '---------------------------------------'
         file = open(filename, 'rw+')
         file.seek(0, 2)
-        file.writelines(result) # test to try make a xml
-       #file.writelines(qqq) # uncoment this to write a rss
-        #file.writelines('\n')
+        file.writelines(qqq) # uncoment this to write a rss
+        file.writelines('\n')
         file.close()
         print '---------------------------------------'
         print '---------------------------------------'
